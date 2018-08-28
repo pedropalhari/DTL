1a768aa9f4c01501ec09e48ef0ababb1b1b1acde
Pedro Palhari <pedropalhari@gmail.com>
2018-08-28 08:16:15 -0300

    organizando estrutura

--- teste.cpp
+++ teste.cpp
@@ -2,16 +2,9 @@
 #include <iostream>
 #include <typeinfo>
 #include <unordered_map>
-#define cast(X) (*any_cast<unordered_map<string, any> *>(X))
-
-#define cast2(X) (*any_cast<unordered_map<string, basicObject> *>(X.obj))
+#define cast(X) (*any_cast<unordered_map<string, basicObject> *>(X.obj))
 
 using namespace std;
-
-class objeto {
- public:
-  unordered_map<string, any> GLOBAL;
-};
 
 typedef struct {
   int type;
@@ -23,25 +16,15 @@ void decast(basicObject x) {
 }
 
 int main() {
-  any GLOBAL = new unordered_map<string, any>();
-  cast(GLOBAL)["a"] = 3;
-  cast(GLOBAL)["b"] = new unordered_map<string, any>();
-  string entry = "abc";
-  cast(cast(GLOBAL)["b"])["c"] = entry;
-  cout << any_cast<int>(cast(GLOBAL)["a"]) << endl;
-  cout << any_cast<string>(cast(cast(GLOBAL)["b"])["c"]) << endl;
-
-  basicObject GLOBAL2;
-  GLOBAL2.type = 0;
-  GLOBAL2.obj = new unordered_map<string, basicObject>();
+  basicObject GLOBAL;
+  GLOBAL.type = 0;
+  GLOBAL.obj = new unordered_map<string, basicObject>();
 
   basicObject obj1;
   obj1.type = 1;
   obj1.obj = 3;
 
-  cast2(GLOBAL2)["a"] = obj1;
-
-  decast(cast2(GLOBAL2)["a"]);
+  cast(GLOBAL)["a"] = obj1;
 
   basicObject obj2;
   obj2.type = 0;
@@ -51,11 +34,12 @@ int main() {
   obj3.type = 1;
   obj3.obj = 4;
 
-  cast2(GLOBAL2)["b"] = obj2;
+  cast(GLOBAL)["b"] = obj2;
 
-  cast2(cast2(GLOBAL2)["b"])["c"] = obj3;
+  cast(cast(GLOBAL)["b"])["c"] = obj3;
 
-  decast(cast2(cast2(GLOBAL2)["b"])["c"]);
+  decast(cast(GLOBAL)["a"]);
+  decast(cast(cast(GLOBAL)["b"])["c"]);
 
   return 0;
 }
